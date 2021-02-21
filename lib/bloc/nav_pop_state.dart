b838fe2f45bd6456eb8b2686dbfacab75af8b459
rr-reddy <v22r18@gmail.com>
2021-02-21 10:10:06 +0530

    removed 3rd party library ( bloc)

--- lib/bloc/nav_pop_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'nav_pop_cubit.dart';
-
-@immutable
-abstract class NavPopState {}
-
-class NavPopInitial extends NavPopState {}
--- lib/routor_page.dart
+++ lib/routor_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_nav/nav_delegates/my_route_information_parser.dart';
 import 'package:flutter_app_nav/nav_delegates/my_router_delegate.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'bloc/nav_pop_cubit.dart';
 
 class RouterPage extends StatefulWidget {
   @override
@@ -17,16 +14,10 @@ class _RouterPageState extends State<RouterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<NavPopCubit>(
-      create: (context) => NavPopCubit(),
-      child: BlocListener<NavPopCubit, DateTime>(
-        listener: (context, state) {},
-        child: MaterialApp.router(
-          routeInformationParser: _myRouteInformationParser,
-          routerDelegate: _myRouterDelegate,
-          title: "Nav 2.0 App",
-        ),
-      ),
+    return MaterialApp.router(
+      routeInformationParser: _myRouteInformationParser,
+      routerDelegate: _myRouterDelegate,
+      title: "Nav 2.0 App",
     );
   }
 }
